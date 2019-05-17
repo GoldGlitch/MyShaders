50f19895678fb7e128cc27071ff2472dad5244e1
GoldGlitch <47577403+GoldGlitch@users.noreply.github.com>
2019-05-17 12:05:57 +0200

    Shaders

---
+++ Shaders/Assets/ShaderGph/Scene/Files/Scripts/AnimateCutout.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace AdvancedDissolve_Example
+{
+    public class AnimateCutout : MonoBehaviour
+    {
+        public bool updateGI;
+
+
+        Renderer mRenderer;
+        Material material;
+
+        float offset;
+        float speed;
+        
+
+        private void Start()
+        {
+            mRenderer = GetComponent<Renderer>();
+
+            material = mRenderer.material;
+
+            offset = Random.value;
+            speed = Random.Range(0.1f, 0.2f);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            material.SetFloat("_DissolveCutoff", Mathf.PingPong(offset + Time.time * speed, 1));
+
+
+            if (updateGI)
+            {
+
+                material.SetVector("_Dissolve_ObjectWorldPos", transform.position);
+
+
+
+                RendererExtensions.UpdateGIMaterials(mRenderer);
+            }
+        }
+    }
+}
---
+++ Shaders3D/Assets/ShaderLabs/BoxMaterial/ShaderControl.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderControl : MonoBehaviour
+{
+
+    public Material Section;
+    public Material Extrude;
+    public float Value;
+    public float MaxValue;
+    public float MinValue;
+    ////private float disolveMax = -1;
+    ////private float disolveMin = .5f;
+    //public bool Inside = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Section.SetFloat("_Val", Value);
+        Extrude.SetFloat("_Factor", Value);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Section
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Value < MaxValue)
+            {
+                Value += .25f;
+                Section.SetFloat("_Val", Value);
+
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (Value > MinValue)
+            {
+                Value -= .25f;
+                Section.SetFloat("_Val", Value);
+            }
+        }
+        #endregion
+        #region Extrude
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (Value < MaxValue)
+            {
+                Value += .05f;
+                Extrude.SetFloat("_Factor", Value);
+
+            }
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (Value > MinValue)
+            {
+                Value -= .05f;
+                Extrude.SetFloat("_Factor", Value);
+            }
+        }
+        #endregion
+    }
+
+
+}
+
